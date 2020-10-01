d1e29c47f31afbfa1b1e97079b5bed5543203003
Alec DuBois <after.alec@gmail.com>
2020-10-01 14:40:27 -0700

    Finishes profile form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,9 +15,8 @@ class UsersController < ApplicationController
   end
 
   def update
-    user = User.find(params[:id])
-    user.update(user_params)
-    render json: user
+    self.current_user.update(user_params)
+    render json: { message: "Update completed successfully" }
   end
 
   def destroy
